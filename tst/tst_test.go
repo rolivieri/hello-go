86a6bdfe01dac0bb538b06ca92d05711a3ae8b2f
Ricardo Olivieri <roliv@us.ibm.com>
2018-01-31 14:22:29 -0600

    tests

---
+++ tst/tst_test.go
@@ -0,0 +1,20 @@
+package tst
+
+// To run test cases here, you can execute:
+// `go test`
+// `go test <name of test file>`, such as go test tst_test.go
+// It looks like the name of the test file must end with _test.go when using `go test`.
+// Here we are leveraging testify for assertions: https://github.com/stretchr/testify
+// go get github.com/stretchr/testify (so remember to fetch this dependency first)
+
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestXYZ (t *testing.T) {
+    var a string = "Hello"
+    var b string = "Hello"  
+    assert.Equal(t, a, b, "The two words should be the same.")
+}
+
