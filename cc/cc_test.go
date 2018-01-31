546785ccb19ae205c40379073b1a980d87b3a72f
Ricardo Olivieri <roliv@us.ibm.com>
2018-01-31 15:52:59 -0600

    tests

---
+++ cc/cc_test.go
@@ -0,0 +1,53 @@
+package main
+
+// Reference: https://medium.com/wearetheledger/about-smart-contract-testing-fbf7b576bb9f
+
+import (
+	"fmt"
+    "testing"
+	"github.com/stretchr/testify/assert"
+	// "encoding/json"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func TestXYZ (t *testing.T) {
+    var a string = "Hello"
+    var b string = "Hello"  
+    assert.Equal(t, a, b, "The two words should be the same.")
+}
+
+func TestCreateNewUser(t *testing.T) {
+	// ARRANGE
+	simpleAsset := new(SimpleAsset)
+	mockStub := shim.NewMockStub("mockstub", simpleAsset)
+	fmt.Println(mockStub)
+	// testStub := TestAPIStub{data: make(map[string][]byte)}
+	txId := "mockTxID"
+	internalId := "id"
+	firstName := "John"
+
+	args := []string {
+		internalId,
+		firstName,
+		"Doe",
+		"john.doek@somewhere.com",
+	 }
+
+	 // ACT
+	 mockStub.MockTransactionStart(txId)
+	 response := simpleAsset.createUser(mockStub, args)
+	 mockStub.MockTransactionEnd(txId)
+
+	// ASSERT
+	if s := response.GetStatus(); s != 200 {
+	   t.Errorf("The response status is %d, instead of 200", s)
+	   t.Errorf("The message: %s", response.Message)
+	}
+ 
+	//TODO
+	// var user User
+	// var found bool
+	// json.Unmarshal(mockStub.State[internalId], &user)
+	// // Now you can assert properties of the user object you stored.
+	// // ...
+ }
