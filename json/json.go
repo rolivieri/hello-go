e7fdb230a61f670d950db564ecc47fd4ebf701db
Ricardo Olivieri <r_olivieri@yahoo.com>
2018-06-19 12:38:37 -0400

    test

--- json/json.go
+++ json/json.go
@@ -3,17 +3,20 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"time"
 )
 
 // User structure
 type User struct {
-	Name  string `json:"name"`
-	Phone string `json:"phone"`
-	Age   int    `json:"age"`
+	Name      string    `json:"name"`
+	Phone     string    `json:"phone"`
+	Age       int       `json:"age"`
+	CreatedTs time.Time `json:"createdTs,string"`
 }
 
 func main() {
-	user1 := &User{Name: "John", Phone: "512-000-000", Age: 35}
+	now := time.Now()
+	user1 := &User{Name: "John", Phone: "512-000-000", Age: 35, CreatedTs: now}
 	b, err := json.Marshal(user1) // b is an array of bytes
 	if err != nil {
 		fmt.Println(err)
@@ -29,5 +32,23 @@ func main() {
 		fmt.Println(error)
 		return
 	}
+
 	fmt.Println(user2)
+
+	var jsonStr = `{
+		"name": "John",
+		"phone": "512-000-000",
+		"age": 35,
+		"createdTs": "2018-06-19T12:29:42.647435475-04:00"
+	  }`
+
+	fmt.Println(jsonStr)
+
+	error = json.Unmarshal([]byte(jsonStr), &user2)
+	if error != nil {
+		fmt.Println(error)
+		return
+	}
+
+	fmt.Printf("%+v\n", user2)
 }
